96363d916f5935d7365cdab5cb2d73fb0481992f
pramirez <pramirez@laserspineinstitute.com>
2018-12-06 08:44:26 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,122 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
+
+
+const profiles = [
+    {
+        id: 1,
+        userID: '1',
+        favoriteMovieID: '1'
+    },
+    {
+        id: 2,
+        userID: '2',
+        favoriteMovieID: '1'
+    },
+    {
+        id: 3,
+        userID: '4',
+        favoriteMovieID: '5'
+    },
+    {
+        id: 4,
+        userID: '5',
+        favoriteMovieID: '2'
+    },
+    {
+        id: 5,
+        userID: '3',
+        favoriteMovieID: '5'
+    },
+    {
+        id: 6,
+        userID: '6',
+        favoriteMovieID: '4'
+    }
+];
+
+const users = {
+    1: {
+        id: 1,
+        name: 'Jane Cruz',
+        userName: 'coder',
+    },
+    2: {
+        id: 2,
+        name: 'Matthew Johnson',
+        userName: 'mpage',
+    },
+    3: {
+        id: 3,
+        name: 'Autumn Green',
+        userName: 'user123',
+    },
+    4: {
+        id: 4,
+        name: 'John Doe',
+        userName: 'user123',
+    },
+    5: {
+        id: 5,
+        name: 'Lauren Carlson',
+        userName: 'user123',
+    },
+    6: {
+        id: 6,
+        name: 'Nicholas Lain',
+        userName: 'user123',
+    },
+};
+
+const movies = {
+    1: {
+        id: 1,
+        name: 'Planet Earth 1',
+    },
+    2: {
+        id: 2,
+        name: 'Selma',
+    },
+    3: {
+        id: 3,
+        name: 'Million Dollar Baby',
+    },
+    4: {
+        id: 4,
+        name: 'Forrest Gump',
+    },
+    5: {
+        id: 5,
+        name: 'Get Out',
+    },
+};
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+          <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">ReactND - Coding Practice</h1>
+          </header>
+          <h2 style={{ marginLeft: `200px` }}>Favorite Movies</h2>
+          { profiles.map(profile => {
+            const userName = users[profile.userID].name;
+            const favMovie = movies[profile.favoriteMovieID].name;
+            const userNameID = users[profile.userID].id;
+
+            return(
+                <div style={{ marginLeft: `200px`}}>
+                  <li>{ userName } "'s favorite movie is " { favMovie }  "---" { userNameID } </li>
+                </div>
+            )
+          })}
+
       </div>
     );
   }
 }
+
 
 export default App;
